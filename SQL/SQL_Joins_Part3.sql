b6aa87200d775253849c69642e0ee5608e9a788b
sakhawatKhan92 <sakhawat.u.khan@gmail.com>
2026-07-26 16:00:16 +0530

    Update SQL_Joins_Part3.sql

--- SQL/SQL_Joins_Part3.sql
+++ SQL/SQL_Joins_Part3.sql
@@ -7,3 +7,14 @@ ORDER BY e.salary DESC;
 SELECT e.emp_name, d.department_name, e.salary
 FROM employees e LEFT JOIN departments d on e.department_id = d.department_id
 WHERE e.salary > 55000;
+
+--Q9 Display the employee name, department name, and salary of employees who belong to either the IT or Finance department.
+SELECT e.emp_name, d.department_name, e.salary
+FROM employees e LEFT JOIN departments d on e.department_id = d.department_id 
+WHERE d.department_name in ("IT","Finance");
+
+--10 -Display all departments along with the number of employees in each department.
+SELECT d.department_name, COUNT(e.emp_id) AS Emp_Count
+FROM employees e RIGHT JOIN departments d ON e.department_id = d.department_id
+GROUP BY d.department_name;
+
