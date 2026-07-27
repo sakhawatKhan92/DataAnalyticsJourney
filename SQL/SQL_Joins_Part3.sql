e7a51785efac8e1b2c5be3685685019ac6636a27
sakhawatKhan92 <sakhawat.u.khan@gmail.com>
2026-07-28 00:53:53 +0530

    Update SQL_Joins_Part3.sql

--- SQL/SQL_Joins_Part3.sql
+++ SQL/SQL_Joins_Part3.sql
@@ -17,4 +17,29 @@ WHERE d.department_name in ("IT","Finance");
 SELECT d.department_name, COUNT(e.emp_id) AS Emp_Count
 FROM employees e RIGHT JOIN departments d ON e.department_id = d.department_id
 GROUP BY d.department_name;
+--Q11 - Display the department name along with the total salary paid to employees in each department.
+SELECT  d.department_name, SUM(e.salary) As Total_Salary
+FROM employees e RIGHT JOIN departments d ON e.department_id = d.department_id
+GROUP BY d.department_name;
+-- Second approach to avoid NULL value
+SELECT  d.department_name, IFNULL(SUM(e.salary),0) As Total_Salary
+FROM employees e RIGHT JOIN departments d ON e.department_id = d.department_id
+GROUP BY d.department_name;
+
+--Q12 - Display the department name and the number of employees whose salary is greater than 50,000 in each department.
+SELECT d.department_name, COUNT(e.emp_id) AS Count_Emp
+FROM departments d LEFT JOIN employees e ON d.department_id = e.department_id
+WHERE Salary > 50000
+GROUP BY d.department_name;
+-- Second approach to "Show all departments and the number of employees earning more than 50,000."
+SELECT d.department_name, 
+COUNT(CASE WHEN e.salary > 50000 THEN e.emp_id END) AS Count_Emp
+FROM departments d LEFT JOIN employees e ON d.department_id = e.department_id
+GROUP BY d.department_name;
+
+--Q14 - Highest Paid Employee-->Display the employee name, department name, and salary of the employee(s) having the highest salary.
+SELECT e.emp_name, d.department_name, e.salary 
+FROM employees e LEFT JOIN departments d ON e.department_id = d.department_id
+WHERE e.salary = (SELECT MAX(salary) FROM employees);
+
 
